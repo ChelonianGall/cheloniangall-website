4d0680ad4801c035eb36e142ccb035877d196c2f
BlaiseEbuth <100444134+BlaiseEbuth@users.noreply.github.com>
2022-08-28 19:35:28 +0200

    Update home.php

--- elements/home.php
+++ elements/home.php
@@ -22,29 +22,28 @@
     <img id = "turtle" src="https://i.giphy.com/media/1qB3EwE3c54A/giphy.webp"/>
     <div id = "about">
         <h2>What is ChelonianGall?</h2>
-        <p>
-            July 11, 2022, <a href = "https://www.codingame.com">CodinGame</a> discontinued their webchat after years of good services.<br/><br/>
-            <a href = "https://cheloniangall.com/">ChelonianGall</a> is here to host that community they don't want.<br/><br/>
-            Here you will find a chat server reachable with the <a href = "https://xmpp.org/software/clients/">XMPP client</a> of your choice as well as with the dedicated webchat.
-        </p>
-        <h2>What's next?</h2>
-        <p>
-            Once the webchat's new visual ready, the next step will be an <a href = "https://greasyfork.org/en">user script</a> to integrate ChelonianGall on CodinGame, where it belongs.<br/><br/>
-            At the same time, we are actively working to save our dear friend <a href = "https://www.codingame.com/blog/markov-chain-automaton2000/">Automaton2000</a> and to give him his right place among us.
-        </p>
-        <h2>Is it open-source?</h2>
-        <p>
-            Yes it is! The ChelonianGall project is currently spread over 3 differents repository:
-            <ul>
-                <li>The server's one. (Private for obvious security reasons.)</li>
-                <li><a href="https://github.com/BlaiseEbuth/cheloniangall-website">The website's one.</a></li>
-                <li><a href="https://github.com/BlaiseEbuth/cheloniangall-automaton2000">The Automaton2000's one.</a></li>
-            </ul>
-            An overview of the project roadmap is available <a href="https://github.com/users/BlaiseEbuth/projects/1">here</a>.<br/>
-        </p>
-        <h2>Issues/Request</h2>
-        <p>
-            If you are encountering any issues or have a feature request, feel free to contact me directly on the chat (@BlaiseEbuth) or to submit an issue (Or even a pull request!) on the corresponding repository.
-        </p>
+        <p>July 11, 2022, <a href="https://codingame.com/">CodinGame</a> discontinued their webchat after years of good services, arguing it was complicated to maintain and moderate. Which was true. But instead of reworking/remade it, they decided to delete it completely and to rely only on their Discord server. A perfect trade from their point of view. But sadly they missed the point.</p>
+        <p>As a mater of fact, the Discord solution has some huge problems:</p>
+        <ul>
+            <li>It's not integrated on the site. Which is problematic for both visibility (for new users) and UX.</li>
+            <li>Some users do not want to create a Discord account, mainly for privacy reasons.</li>
+            <li>A lot of users used to connect to the chat from school/work and Discord is blocked in lot of these places.</li>
+            <li>In the "Clash of Code" mode a new chat room was created for each game, which Discord can not handle.</li>
+        </ul>
+        <p>ChelonianGall takes the form of a <a href="https://xmpp.org/">XMPP</a> server coupled with a dedicated webchat. The project goal is to offer a community driven alternative to the CodinGame's Discord server while solving some of the above-mentioned problems:</p>
+        <ul>
+            <li>The inscription only require a nickname and a password.</li>
+            <li>The use of XMPP and of a dedicated domain name drastically reduce the risk to see it blocked by a firewall.</li>
+            <li>An <a href="https://greasyfork.org/en">user script</a> to integrate our webchat directly on CodinGame is under development.</li>
+        </ul>
+        <p>Don't hesitate to join us!</p>
+        <h2>Project status</h2>
+        <p>The XMPP server is up and can be reached trough the dedicated webchat located at <a href="https://cheloniangall.com/">cheloniangall.com</a> or via the your favorite <a href="https://xmpp.org/software/clients/">XMPP client</a>.</p>
+        <p>We are currently working on a full new design for the webchat in the form of a right-sided column, as was the CodinGame's one. Then, we plan to create an user script to allow the integration of our chat on CodinGame.</p>
+        <p>After that, the next step will be to save our dear friend Automaton2000, and to give him his right place among us.</p>
+        <p>An overview of the project status is available <a href="https://github.com/orgs/ChelonianGall/projects/1">here.</a></p>
+        <h2>report and contribution</h2>
+        <p>If you are encountering any issues or have a feature request, feel free to ask directly on the chat or to submit an issue on the corresponding <a href="https://github.com/orgs/ChelonianGall/repositories">repository</a>. Same if you want to help on the development.</p>
+        <p>Thanks in advance!</p>
     </div>
 </div>
